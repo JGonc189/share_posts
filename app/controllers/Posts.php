e68d5c9e656aebe50d8f30e97ea36ad3bc4f69d0
Jason Goncalves <jgoncalves@protonmail.com>
2018-10-07 22:11:16 -0400

    added insert post functionality

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -23,11 +23,52 @@
         }
 
         public function add() {
-            $data = [
-                'title' => '',
-                'body' => ''
-            ];
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                // sanitize the post
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                $data = [
+                    'title' => trim($_POST['title']),
+                    'body' => trim($_POST['body']),
+                    'user_id' => $_SESSION['user_id'],
+                    'title_err' => '',
+                    'body_err' => ''
+                ];
+
+                // Validate data
+                if(empty($data['title'])) {
+                    $data['title_err'] = 'Please enter Title...';
+                }
+                
+                if(empty($data['body'])) {
+                    $data['body_err'] = 'Please enter Body Text....';
+                }
+
+                // make sure there are no errors
+                if(empty($data['title_err']) && empty($data['body_err'])) {
+                    // validated
+                    if($this->postModel->addPost($data)){
+                        flash('post_message', 'Post Added');
+                        redirect('posts');
+                    } else {
+                        die('Something went wrong');
+                    }
+
+                } else {
+                    // load the view
+                    $this->view('posts/add', $data);
+                }
+
+            } else {
+                $data = [
+                    'title' => '',
+                    'body' => ''
+                ];
+    
+                $this->view('posts/add', $data);
+
+            }
+
             
-            $this->view('posts/add', $data);
         }
     }
